5dd17cbc9841b8caa97f878f4ace244324768464
johanbert <johanbertmosquera@gmail.com>
2021-03-27 21:42:03 -0500

    deleting comments

--- server/app.js
+++ server/app.js
@@ -6,9 +6,3 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(require('./routes/index'));
 app.listen(process.env.PORT, () => console.log(`Listening PORT:${process.env.PORT}`));
-
-// const jwt = require('jsonwebtoken');
-// app.get('/api/jwt', (req, res) => {
-//     let token = jwt.sign({}, process.env.SECRET)
-//     res.json({ token });
-// })
